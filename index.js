1bc86e973889c42d3efadb621e5a71c4c7f548de
Sovetov Birzhan <bsovetov301@gmail.com>
2025-04-09 17:31:50 +0500

    added Show events

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import ShowEvents from './ShowEvents';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ShowEvents/>
+    
   </React.StrictMode>
 );
